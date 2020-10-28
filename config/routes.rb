179ec1d72c0208c3abfec6cff827f1c7e85bede4
Poly-Jiro <never_disappear_this_ties@yahoo.co.jp>
2020-10-28 18:15:33 +0900

    コメント機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get 'prototypes/index', to: "prototypes#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "prototypes#index"
-  resources :prototypes
+  resources :users, only: :show
+  resources :prototypes do
+    resources :comments, only: :create
+  end
 end
